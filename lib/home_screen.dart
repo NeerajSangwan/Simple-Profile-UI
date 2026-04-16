f2f045cc94796a26bf95b5736b85652eff07339f
NeerajSangwan <neerajjitender777@gmail.com>
2026-04-16 13:09:45 +0530

    Added screenshot

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: Stack(
           children: [
